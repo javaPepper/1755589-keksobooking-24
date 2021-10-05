4055011dfce22aca11433a08a7e3c8df77607d28
Ананьев Сергей <9852407799@mail.ru>
2021-10-05 17:24:32 +0300

    1я версия дз

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+let avatarAddress = img/avatars/user/01.png; // написать функцию добавления числа от 01 до 10 
+let result = 0;
+let roomType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+let roomsNum = 0;
+let guestsNum = 0;
+const checkinValue = ['12:00', '13:00', '14:00'];
+const checkoutValue = ['12:00', '13:00', '14:00'];
+let featuresValue = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+let photosValue = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+let locationValue = {
+lat: getRandomNumDecimal(35.65000, 35.70000, 5),
+lng: getRandomNumDecimal(139.70000, 139.80000, 5)
+};
+
 function getRandomNum(from, to) {
 
   from = Math.ceil(from); // округляем минимальное число в большую сторону
@@ -17,22 +33,42 @@ function getRandomNum(from, to) {
   return result;
 }
 
-getRandomNum(5, 22);
 
 function getRandomNumDecimal(from, to, decimal) {
 
-  let result = Math.random() * (to - from) + from; // вычисляем любое число с плавающей точкой в диапазоне включительно
+  let resultFloat = Math.random() * (to - from) + from; // вычисляем любое число с плавающей точкой в диапазоне включительно
 
   if (from >= to) { // проверяем, что параметр "до" больше параметра "от"
-    result = false;
+    resultFloat = false;
 
   }
   if (from<0||to<0) { // проверяем, что параметры больше 0
-    result = false;
+    resultFloat = false;
 
   }
-  result = result.toFixed(decimal); // задаем параметр decimal
-  return result;
+  resultFloat = resultFloat.toFixed(decimal); // задаем параметр decimal
+  return resultFloat;
 }
 
-getRandomNumDecimal(5, 12, 5);
+let author = {
+  avatar: avatarAddress 
+}
+  
+
+let offer = {
+  title: '1-е объявление',
+  address: '{{location.lat}}, {{location.lng}}',
+  price: result,
+  type: roomType,
+  rooms: roomsNum,
+  guests: guestsNum,
+  checkin: checkinValue,
+  checkout: checkoutValue,
+  features: featuresValue,
+  description: 'Комната',
+  photos: photosValue,
+  location: locationValue
+}
+offer.price = getRandomNum(100, 50000);
+offer.roomsNum = getRandomNum(1, 4);
+offer.guestsNum = getRandomNum(1, 8);
