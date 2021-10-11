ca1b1f046036910b447fba9f2226efd6873d9213
Ананьев Сергей <9852407799@mail.ru>
2021-10-11 18:11:10 +0300

    Ошибка, которую не могу найти

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-let avatarAddress = img/avatars/user/01.png; // написать функцию добавления числа от 01 до 10 
-let result = 0;
-let roomType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-let roomsNum = 0;
-let guestsNum = 0;
-const checkinValue = ['12:00', '13:00', '14:00'];
-const checkoutValue = ['12:00', '13:00', '14:00'];
-let featuresValue = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-let photosValue = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-let locationValue = {
-lat: getRandomNumDecimal(35.65000, 35.70000, 5),
-lng: getRandomNumDecimal(139.70000, 139.80000, 5)
-};
-
 function getRandomNum(from, to) {
 
   from = Math.ceil(from); // округляем минимальное число в большую сторону
@@ -31,8 +15,7 @@ function getRandomNum(from, to) {
   }
 
   return result;
-}
-
+};
 
 function getRandomNumDecimal(from, to, decimal) {
 
@@ -48,27 +31,81 @@ function getRandomNumDecimal(from, to, decimal) {
   }
   resultFloat = resultFloat.toFixed(decimal); // задаем параметр decimal
   return resultFloat;
-}
+};
 
-let author = {
-  avatar: avatarAddress 
-}
-  
+function getRandomLength() {
+  randomFeatures = Math.ceil(Math.random() * (featuresArray.length - 0) + 0);
+  return randomFeatures;
+};
 
-let offer = {
+function  getRandomFeatures(){
+for (let i = 0; i<=featuresNum; i++) {
+  features[i]  = featuresArray[i];
+}
+ return features;
+};
+
+function getRandomLengthPhotos() {
+  randomPhotos = Math.ceil(Math.random() * (photosArray.length - 0) + 0);
+  return randomPhotos;
+};
+
+function  getRandomPhotos(){
+
+for (let i = 0; i<=photosNum; i++) {
+photos[i] = photosArray[i]
+}
+ return photos;
+};
+
+let firstAd = {
+
+avatarNum: '0' + 1,
+avatarAddress: 'url(img/avatars/user/' + avatarNum + '.png)', // написать функцию добавления числа от 01 до 10
+//addressValueLat,
+//addressValueLng,
+addressValueLat: getRandomNumDecimal(35.65000, 35.70000, 5),
+addressValueLng: getRandomNumDecimal(139.70000, 139.80000, 5),
+result: 0,
+roomType: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+roomsNum: 0,
+guestsNum: 0,
+checkinValue: ['12:00', '13:00', '14:00'],
+checkoutValue: ['12:00', '13:00', '14:00'],
+featuresValue: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+photosArray: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
+locationValue: {
+lat: getRandomNumDecimal(35.65000, 35.70000, 5),
+lng: getRandomNumDecimal(139.70000, 139.80000, 5)
+},
+featuresArray: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+features: [],
+randomFeatures,
+featuresNum: getRandomLength(),
+photos: [],
+randomPhotos,
+photosNum: getRandomLengthPhotos(),
+
+author: {
+  avatar: avatarAddress
+},
+offer: {
   title: '1-е объявление',
-  address: '{{location.lat}}, {{location.lng}}',
+  address: addressValueLat + ' ' + addressValueLng,
   price: result,
   type: roomType,
   rooms: roomsNum,
   guests: guestsNum,
   checkin: checkinValue,
   checkout: checkoutValue,
-  features: featuresValue,
+  features: getRandomFeatures(),
   description: 'Комната',
-  photos: photosValue,
+  photos: getRandomPhotos(),
   location: locationValue
-}
-offer.price = getRandomNum(100, 50000);
-offer.roomsNum = getRandomNum(1, 4);
-offer.guestsNum = getRandomNum(1, 8);
+},
+firstAd.offer.price = getRandomNum(100, 50000),
+firstAd.offer.rooms = getRandomNum(1, 4),
+firstAd.offer.guests = getRandomNum(1, 8)
+};
