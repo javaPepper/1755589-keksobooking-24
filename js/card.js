54b3792f87418aee6cea97197b8c12e6473fb802
Ананьев Сергей <9852407799@mail.ru>
2021-10-27 00:28:08 +0300

    hidden

--- js/card.js
+++ js/card.js
@@ -1,10 +1,12 @@
-//const log = console.log;
+const log = console.log;
+let rooms = 'комнаты';
+let guests = 'гостей';
 const mapCanvas = document.querySelector('#map-canvas');
 
 const similarCardTemplate = document.querySelector('#card').content;
 const cardElement = similarCardTemplate.cloneNode(true);
 
-const popup = similarCardTemplate.querySelector('.popup');
+const popup = cardElement.querySelector('.popup');
 const popupAvatar = popup.querySelector('.popup__avatar');
 const popupTitle = popup.querySelector('.popup__title');
 const popupAddress = popup.querySelector('.popup__text--address');
@@ -12,11 +14,14 @@ const popupPrice = popup.querySelector('.popup__text--price');
 const popupType = popup.querySelector('.popup__type');
 const popupCapacity = popup.querySelector('.popup__text--capacity');
 const popupTime = popup.querySelector('.popup__text--time');
-const popupFeatures = popup.querySelector('.popup__features');
-const popupFeaturesCollection = popupFeatures.children;
-const popupDescription = popup.querySelector('.popup__description');
-const popupPhotos = popup.querySelector('.popup__photos');
+const popupFeatures = popup.querySelector('.popup__feature--wifi');
 
+//const popupFeaturesCollection = popupFeatures.children;
+//log(popupFeatures[2]);
+//const popupFeaturesCollection = popup.querySelectorAll('.popup__feature');
+const popupDescription = popup.querySelector('.popup__description');
+const popupPhotos = popup.querySelector('.popup__photo');
+//
 const renderCards = function (object) {
 
   popupAvatar.src = object.author.avatar;
@@ -39,18 +44,27 @@ const renderCards = function (object) {
   if (object.offer.type === 'hotel') {
     popupType.innerHTML = 'Отель';
   }
-  popupCapacity.innerHTML = `${object.offer.rooms  } комнаты для ${  object.offer.guests  } гостей`;
+  if (object.offer.rooms === 1) {
+    rooms = 'комната';
+  }
+  if (object.offer.guests === 1) {
+    guests = 'гостя';
+  }
+  popupCapacity.innerHTML = `${object.offer.rooms  } ${  rooms  } ` + 'для' + ` ${  object.offer.guests  } ${  guests}`;
+
   popupTime.innerHTML = `Заезд после ${  object.offer.checkin  }, выезд до ${  object.offer.checkout}`;
 
-  for (let i = 0; i<6; i++) {
+  popupFeatures.hidden = true;
+  /*for (let i = 0; i<object.offer.features.length; i++) {
     popupFeaturesCollection[i].innerHTML = object.offer.features[i];
     if (popupFeaturesCollection[i].innerHTML === '') {
-      popupFeaturesCollection.hidden = true;
+      popupFeaturesCollection[i].hidden = true;
     }
-  }
+    //log(popupFeaturesCollection[i].innerHTML);
+  }*/
   popupDescription.innerHTML= object.offer.description;
-  popupPhotos.innerHTML = object.offer.photos;
-  for (let i = 0; i<2; i++) {
+
+  for (let i = 0; i<3; i++) {
     popupPhotos.src = object.offer.photos[i];
   }
   mapCanvas.appendChild(cardElement);
