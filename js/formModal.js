9d6b4473c9aab540529d09861455b4ddef7f0e15
Ананьев Сергей <9852407799@mail.ru>
2021-11-04 01:28:55 +0300

    добавил валидацию заголовку

--- js/formModal.js
+++ js/formModal.js
@@ -3,6 +3,9 @@ const adFormHeader = adForm.querySelector('.ad-form-header');
 const adFormElement = adForm.querySelectorAll('.ad-form__element');
 const mapFilters = document.querySelector('.map__filters');
 const mapFiltersElement = mapFilters.querySelectorAll('.map__filter');
+const title = adForm.querySelector('#title');
+const MIN_TITLE_VALUE_LENGTH = 30;
+const MAX_TITLE_VALUE_LENGTH = 100;
 
 const disableForms = function() {
   adForm.classList.add('ad-form--disabled');
@@ -26,4 +29,19 @@ const enableForms = function() {
     mapFiltersElement[j].disabled = false;
   }
 };
-export{enableForms, disableForms};
+
+const titleListener = title.addEventListener('input',  () => {
+  const valueLength = title.value.length;
+  if (valueLength<MIN_TITLE_VALUE_LENGTH) {
+    title.setCustomValidity(`Добавьте еще ${  MIN_TITLE_VALUE_LENGTH - valueLength}   симв.`);
+  }
+  else if (valueLength>MAX_TITLE_VALUE_LENGTH) {
+    title.setCustomValidity(`Уберите ${  valueLength - MAX_TITLE_VALUE_LENGTH  } симв.`);
+  }
+  else {title.setCustomValidity('');
+  }
+  title.reportValidity();
+});
+
+
+export{enableForms, disableForms, titleListener};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {createAdObject} from './data.js';
 import {renderCards} from './card.js';
+import {enableForms, disableForms, titleListener} from './formModal.js';
 
 let firstObject = createAdObject();
 firstObject = firstObject[0];
 
 renderCards(firstObject);
-
+disableForms();
+enableForms();
+titleListener;
