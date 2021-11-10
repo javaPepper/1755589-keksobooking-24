4113e4da55e1841b67c7f7e8d51b0dc22c1f4f86
Ананьев Сергей <9852407799@mail.ru>
2021-11-10 23:39:27 +0300

    решение конфликта

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {createAdObject} from './data.js';
 import {renderCards} from './card.js';
-import {enableForms, disableForms, setDisableOptions, setEnableOptions} from './formModal.js';
+import {enableForms, disableForms} from './formModal.js';
 
 let firstObject = createAdObject();
 firstObject = firstObject[0];
@@ -8,7 +8,3 @@ firstObject = firstObject[0];
 renderCards(firstObject);
 disableForms();
 enableForms();
-setDisableOptions();
-setEnableOptions();
-
-
